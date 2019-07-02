8dc43f4e9029f29b73ea591154015d1d18cb4f04
Adam Rais <raisadamo@gmail.com>
2019-07-02 15:01:28 +0900

    Add canvas implementation

--- src/lib.rs
+++ src/lib.rs
@@ -49,6 +49,25 @@ impl Universe {
   }
 }
 
+#[wasm_bindgen]
+impl Universe {
+  pub fn render(&self) -> String {
+    self.to_string()
+  }
+
+  pub fn width(&self) -> u32 {
+    self.width
+  }
+
+  pub fn height(&self) -> u32 {
+    self.height
+  }
+
+  pub fn cells(&self) -> *const Cell {
+    self.cells.as_ptr()
+  }
+}
+
 // Access a cell at a given row and column
 impl Universe {
   fn get_index(&self, row: u32, column: u32) -> usize {
