fd1b87dddd7933a8926c468f92f4d7f71baf6c38
Adam Rais <raisadamo@gmail.com>
2019-07-02 10:49:37 +0900

    Add Universe implemenation

--- src/lib.rs
+++ src/lib.rs
@@ -9,11 +9,25 @@ use wasm_bindgen::prelude::*;
 static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
 
 #[wasm_bindgen]
-extern {
-    fn alert(s: &str);
+#[repr(u8)]
+#[derive(Clone, Copy, Debug, PartialEq, Eq)]
+pub enum Cell {
+  Dead = 0,
+  Alive = 1,
 }
 
 #[wasm_bindgen]
-pub fn greet() {
-    alert("Hello, wasm-game-of-adz!");
+pub struct Universe {
+  width: u32,
+  height: u32,
+  cells: Vec<Cell>,
 }
+
+// Access a cell at a given row and column
+impl Universe {
+  fn get_index(&self, row: u32, column: u32) -> usize {
+    (row * self.width + column) as usize
+  }
+}
+
+// Count number of live neighbors
