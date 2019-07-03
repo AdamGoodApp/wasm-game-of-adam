c06817c14f6b622495b3300a545a133356ae4325
Adam Rais <raisadamo@gmail.com>
2019-07-03 10:51:13 +0900

    Add tests for tick

--- src/lib.rs
+++ src/lib.rs
@@ -68,35 +68,20 @@ impl Universe {
   pub fn cells(&self) -> *const Cell {
     self.cells.as_ptr()
   }
-}
 
-// Access a cell at a given row and column
-impl Universe {
-  fn get_index(&self, row: u32, column: u32) -> usize {
-    (row * self.width + column) as usize
+  /// Set the width of the universe.
+  /// Resets all cells to the dead state.
+  pub fn set_width(&mut self, width: u32) {
+    self.width = width;
+    self.cells = (0..width * self.height).map(|_i| Cell::Dead).collect();
   }
-}
 
-// Count number of live neighbors
-impl Universe {
-  fn live_neighbor_count(&self, row: u32, column: u32) -> u8 {
-    let mut count = 0;
-
-    for delta_row in [self.height - 1, 0, 1].iter().cloned() {
-      for delta_col in [self.width - 1, 0, 1].iter().cloned() {
-        if delta_row == 0 && delta_col == 0 {
-          continue;
-        }
-
-        let neighbor_row = (row + delta_row) % self.height;
-        let neighbor_col = (column + delta_col) % self.width;
-        let idx = self.get_index(neighbor_row, neighbor_col);
-
-        count += self.cells[idx] as u8;
-      }
-    }
-
-    count
+  /// Set the height of the universe.
+  ///
+  /// Resets all cells to the dead state.
+  pub fn set_height(&mut self, height: u32) {
+    self.height = height;
+    self.cells = (0..self.width * height).map(|_i| Cell::Dead).collect();
   }
 }
 
@@ -134,6 +119,52 @@ impl Universe {
     }
 
     self.cells = next;
+  }
+}
+
+// Access a cell at a given row and column
+impl Universe {
+  fn get_index(&self, row: u32, column: u32) -> usize {
+    (row * self.width + column) as usize
+  }
+}
+
+// Count number of live neighbors
+impl Universe {
+  fn live_neighbor_count(&self, row: u32, column: u32) -> u8 {
+    let mut count = 0;
+
+    for delta_row in [self.height - 1, 0, 1].iter().cloned() {
+      for delta_col in [self.width - 1, 0, 1].iter().cloned() {
+        if delta_row == 0 && delta_col == 0 {
+          continue;
+        }
+
+        let neighbor_row = (row + delta_row) % self.height;
+        let neighbor_col = (column + delta_col) % self.width;
+        let idx = self.get_index(neighbor_row, neighbor_col);
+
+        count += self.cells[idx] as u8;
+      }
+    }
+
+    count
+  }
+}
+
+impl Universe {
+  // Get the dead and alive values of the entire universe
+  pub fn get_cells(&self) -> &[Cell] {
+    &self.cells
+  }
+
+  /// Set cells to be alive in a universe by passing the row and column
+  /// of each cell as an array.
+  pub fn set_cells(&mut self, cells: &[(u32, u32)]) {
+    for (row, col) in cells.iter().cloned() {
+      let idx = self.get_index(row, col);
+      self.cells[idx] = Cell::Alive;
+    }
   }
 }
 
